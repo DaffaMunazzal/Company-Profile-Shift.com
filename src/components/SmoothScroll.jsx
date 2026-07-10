4eb29edc088d087701846c90336a4debca0b3357
bidzz <abidmuhammadrafi400@gmail.com>
2026-07-10 11:52:03 +0700

    Add files via upload

---
+++ src/components/SmoothScroll.jsx
@@ -0,0 +1,16 @@
+import { ReactLenis } from "lenis/react";
+
+export default function SmoothScroll({ children }) {
+  return (
+    <ReactLenis
+      root
+      options={{
+        duration: 1.2,
+        smoothWheel: true,
+        touchMultiplier: 1.2,
+      }}
+    >
+      {children}
+    </ReactLenis>
+  );
+}
