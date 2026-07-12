19ffc4f588ce411dc441da4b9572fe8a3958f805
M.Khoirun Nabil <117891264+MKhoirunNabil@users.noreply.github.com>
2026-07-12 22:23:51 +0700

    Update contactData.js

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -1,13 +1,13 @@
 export const PRIMARY_HUB = {
   coordinates: [-122.4014, 37.7899],
   addressLines: [
-    "Jl. Maninjau Keputran, Pekalongan, Jawa Tengah, Indonesia",
+    "Blimbing Rt 01/06, Wonorejo, Polokarto, Sukoharjo, Jawa Tengah, Indonesia",
   ],
   mapQuery: "ShiftComp, Pekalongan, Jawa Tengah",
 };
 
 export const SOCIAL_LINKS = [
-  { id: "instagram", label: "Instagram", href: "https://www.instagram.com/khoirunnabil1076/" },
+  { id: "instagram", label: "Instagram", href: "https://www.instagram.com/shift.comp" },
   { id: "twitter", label: "X (Twitter)", href: "https://www.twitter.com/khoirunnabil1076" },
   { id: "linkedin", label: "LinkedIn", href: "https://www.linkedin.com/company/nabilemon" },
   { id: "youtube", label: "YouTube", href: "https://www.youtube.com/@nabil-emon" },
